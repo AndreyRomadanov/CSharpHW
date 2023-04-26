56962e604d9a4a342c71896159aa0f2aa2c86d7c
test-github-04 <test-github-04>
2023-04-26 16:24:28 +0500

    Две задачки готово.

--- CSharpHW2ceminar/CSharp001/Program.cs
+++ CSharpHW2ceminar/CSharp001/Program.cs
@@ -4,25 +4,26 @@
 // 782 -> 8
 // 918 -> 1
 
-SecondElemThreeDigitNumber();
+SecondElemThreeDigitNumber();                                                   // Запускаем решение задачи.
 
-void SecondElemThreeDigitNumber()
+void SecondElemThreeDigitNumber()                                               // Основной метод решения задачи.
 {
-int rand1 = new Random ().Next ( 100 , 1000 ) ;
-Console.WriteLine ( " Случайное трёхзначное число :  " + rand1 ) ;
-int result1 = indexarray ( rand1 ) ;
-Console.WriteLine ( " Второй злемент в трёхзначном числе :  " +  result1   ) ; 
+int rand1 = new Random ().Next ( 100 , 1000 ) ;                                 // Генератор сучайного числа.
+Console.WriteLine ( " Случайное трёхзначное число :  " + rand1 ) ;              // Записываем сгенерированное число.
+int result1 = indexarray ( rand1 ) ;                                            // Второй элемент трёхзначного числа.
+Console.WriteLine ( " Второй злемент в трёхзначном числе :  " +  result1   ) ;  // Записываем результат задачи.
 }
-int indexarray ( int rand ) 
+
+int indexarray ( int rand )                                                     // Метод решения задачи.
 {
-int count = 0 ;
-string str = rand.ToString () ; 
-int [] array = new int [str.Length] ;
-while ( count < str.Length )
+int count = 0 ;                                                                 // Вводим переменную.
+string str = rand.ToString () ;                                                 // Переводим число из int в string.
+int [] array = new int [str.Length] ;                                           // Прописываем строки как массив чисел.  
+while ( count < str.Length )                                                    // Пока переменная меньше конечного индекса массива. (Цикл)
 {
-  array [ count ] = int.Parse ( str [ count ] + " " )  ;
-    count ++ ;
+  array [ count ] = int.Parse ( str [ count ] + " " )  ;                        // Считываем строки по индексу , переводим в int.
+    count ++ ;                                                                  // Увеличиваем переменную на один.
 } 
-int result = array [ 1 ] ;
- return result ;
+int result = array [ 1 ] ;                                                      // Присваиваем результату элемент с индксом один.
+ return result ;                                                                // Возвращаем результат в основной метод.
 } 
