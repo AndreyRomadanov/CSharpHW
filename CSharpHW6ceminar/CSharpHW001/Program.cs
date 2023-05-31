7e6c96b2cc7f97db70111d11c211c3897ad2f888
test-github-04 <test-github-04>
2023-05-31 14:29:45 +0500

    Домашнее задание к шестому семинару.

---
+++ CSharpHW6ceminar/CSharpHW001/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+NumbersArray ( ) ;                                                 // Решаем задачу.
+
+void NumbersArray ( )                                               // Определяем количество в массиве чисел больше 0.
+{
+int[] NumbersArray =  BildSizeBodyArray ( ) ; 
+int even = 0 ;
+for ( int i = 0 ; i < NumbersArray.Length ; i ++ )
+{
+if ( NumbersArray [ i ]  > 0 ) 
+{
+     Console.WriteLine ( $"   Число   { NumbersArray [ i ]  }   - бщльше 0.   " ) ;
+     even ++ ;
+}
+else 
+{
+     Console.WriteLine ( $"   Число   { NumbersArray [ i ]  }   - меньше иои равно 0.   " ) ; 
+} 
+}
+Console.WriteLine ( $" Количество чисел больше 0  =   { even } " ) ;
+}
+
+int[] BildSizeBodyArray ( )                                       // Задаём параметры массива .
+{
+int min = -100;
+int max = 100 ;
+Console.Write ( " Введите  количество чисел в массиве M :  ") ;
+int realsize = int.Parse ( Console.ReadLine ()! ) ;
+int[] array = BildArray  (  min ,  max , realsize ) ; 
+return array ;
+}
+
+int[] BildArray ( int minValue , int maxValue , int tipeSize )    // Создаём массив случайных чисел.
+{
+    int[] arr = new int [ tipeSize ] ;
+    for ( int i = 0 ; i < tipeSize ; i ++ )
+    {
+        arr [ i ] = new Random().Next( minValue , maxValue + 1 ) ;
+    }
+     Console.WriteLine ( $" Создаём массив случайных чисел   [ { String.Join ( " " , arr ) } ] " ) ;
+    return arr ;
+}
---
+++ CSharpHW6ceminar/CSharpHW002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+distbetwtwopoints(); // Применяем метод.Решаем задачу.
+
+void distbetwtwopoints() // Метод. Ввод данных , решение задачи.
+{
+    double b1 = promt(" Введите значение b1 первой точки   :    ");
+    double k1 = promt(" Введите значение k1 первой точки   :    ");
+    double b2 = promt(" Введите значение b2 второй точки   :    ");
+    double k2 = promt(" Введите значение k2 второй точки   :    ");
+    FindtraightLine(b1, k1, b2, k2);
+}
+
+int promt(string message) // Метод. Просим ввести значение,запоминаем.
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void FindtraightLine(double b, double k, double a, double c) // Решаем уравнение.
+{
+    double x = (a - b) / (k - c);
+    double y = k * x + b;
+    Console.WriteLine($" Точка пересечения заданных прямых   :  X =  {x}  ,  Y =  {y} ");
+}
