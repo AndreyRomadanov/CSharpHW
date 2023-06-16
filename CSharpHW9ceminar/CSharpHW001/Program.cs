267a52b893efad2cc7b041da39b6e9bb3e62334f
test-github-04 <test-github-04>
2023-06-16 22:05:42 +0500

    Домашнее задание к девятому семинару.

---
+++ CSharpHW9ceminar/CSharpHW001/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 5, 6, 7, 8"*/
+
+
+BodyBild ( ) ;
+
+void BodyBild ( )                                       // В этом методе вводим данные и решаем задачу методом рекурсии.
+{
+    Console.Write ( " Введите число M  : " ) ;
+    int M = int.Parse ( Console.ReadLine ( ) ! ) ;
+    Console.Write ( " Введите число N  : " ) ;
+    int N = int.Parse ( Console.ReadLine ( ) ! ) ;
+    if ( N > M ) 
+      {
+         EnumerationEvenNumbers ( N , M ) ;            
+      } 
+     else 
+      {
+         EnumerationEvenNumbers ( M , N ) ;
+      }
+}
+
+void EnumerationEvenNumbers ( int count , int i )        // Этим методом ( рекурсия ) перебираем числа в указанном промежутке,
+{                                                        // выводим на экран чётные числа.
+    while ( count >= i )
+    {
+       if ( i % 2 == 0 )
+       {
+         Console.Write ( $" { i } " ) ; 
+         i = i + 1 ; 
+       }
+         EnumerationEvenNumbers ( count , i + 1 ) ;       
+    return;
+    }
+}
---
+++ CSharpHW9ceminar/CSharpHW002/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+BodyBild ( ) ;
+
+void BodyBild ( )                                                 // В этом методе вводим данные и решаем задачу методом рекурсии.
+{
+    Console.Write ( " Введите число M  : " ) ;
+    int M = int.Parse ( Console.ReadLine ( ) ! ) ;
+    Console.Write ( " Введите число N  : " ) ;
+    int N = int.Parse ( Console.ReadLine ( ) ! ) ;
+    int summa = 0 ;
+     if ( N > M ) 
+      {
+         EnumerationEvenNumbers ( N , M , summa ) ;            
+      } 
+     else 
+      {
+         EnumerationEvenNumbers ( M , N , summa ) ;
+      }
+}
+
+void EnumerationEvenNumbers ( int count , int i , int sum)        // Этим методом ( рекурсия ) перебираем числа в указанном промежутке,
+{                                                                 // находим сумму этих чисел.
+    while ( count >= i )
+    {  
+        sum =sum + i ;   
+        EnumerationEvenNumbers ( count , i + 1  , sum ) ;       
+     return;
+    }
+    Console.Write ( $" Сумма всех натуральных чисел между значениями  M и N = { sum }. " ) ; 
+}
